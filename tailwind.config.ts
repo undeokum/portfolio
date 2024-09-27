abc2a44e0936b1e47afb9987027a0a0a8b9d2223
Junseok Lim <limjunseokdev@gmail.com>
2024-09-27 20:43:47 +0900

    20240927

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,48 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import * as colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    extend: {},
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
     },
+    fontFamily: {
+      regular: ['regular'],
+      medium: ['medium'],
+      light: ['light'],
+      extra_light: ['extra_light'],
+      thin: ['thin'],
+      bold: ['bold'],
+      semi_bold: ['semi_bold'],
+      extra_bold: ['extra_bold'],
+      black: ['black'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      red: colors.red,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      slate: colors.slate,
+      orange: colors.orange,
+      blue: colors.blue,
+
+      cblue: '#7194FF'
+    }
   },
   plugins: [],
-};
-export default config;
+}
+export default config
